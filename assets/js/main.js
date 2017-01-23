2237032cb56c79d5eaf4d1436f55bfa22e489d14
Robert Boes <bitbucket@robertboes.nl>
2017-01-23 20:49:04 +0100

    Updated main - Dynamically loading all "applications"

--- assets/js/main.js
+++ assets/js/main.js
@@ -45,6 +45,11 @@ var PlexRedirect = function() {
         UP: 1,
         DOWN: 2
     };
+
+    this.default_messages = {
+        server_up: "Up and reachable",
+        server_down: "Down and unreachable"
+    };
     this.default_config = {
         server: {
             name: "",
@@ -57,26 +62,54 @@ var PlexRedirect = function() {
             server_down: "Down and unreachable"
         }
     };
-    this.default_applications = {
-        PlexWeb: {
-            url: 'http://app.plex.tv/web/app',
-            image: 'assets/img/s01.png',
-            header: 'Access <span class="server-name">SERVER</span>',
-            description: 'Access the <span class="server-name">SERVER</span> library with over X Movies & X TV Shows available instantly.'
+    this.default_applications = [
+        {
+            PlexWeb: {
+                url: 'http://app.plex.tv/web/app',
+                image: 'assets/img/plex.png',
+                header: 'Access <span class="server-name">SERVER</span>',
+                description: 'Access the <span class="server-name">SERVER</span> library with over <span class="server-movies">1000</span> Movies & <span class="server-tv">500</span> TV Shows available instantly.'
+            },
+            PlexRequests: {
+                url: document.domain + ':3000',
+                image: 'assets/img/plexrequests.png',
+                header: 'Requests',
+                description: 'Want to watch a Movie or TV Show but it\'s not currently on <span class="server-name">SERVER</span>? Use PlexRequests to request it!'
+            },
+            Ombi: {
+                url: document.domain + ':3579',
+                image: 'assets/img/ombi.png',
+                header: 'Requests',
+                description: 'Want to watch a Movie or TV Show but it\'s not currently on <span class="server-name">SERVER</span>? Use Ombi to request it!'
+            },
+            PlexEmail: {
+                url: document.domain + '#PLEXEMAIL_LINK_HERE',
+                image: 'assets/img/plexemail.png',
+                header: 'What\'s New',
+                description: 'See what has been recently added to <span class="server-name">SERVER</span> without having to log in.'
+            }
         },
-        PlexRequests: {
-            url: '#PLEXREQUESTS_LINK_HERE',
-            image: 'assets/img/s02.png',
-            header: 'Request',
-            description: 'Want to watch a Movie or TV Show but it\'s not currently on <span class="server-name">SERVER</span>? Request it here!'
-        },
-        PlexEmail: {
-            url: '#PLEXEMAIL_LINK_HERE',
-            image: 'assets/img/s03.png',
-            header: 'What\'s New',
-            description: 'See what has been recently added to <span class="server-name">SERVER</span> without having to log in.'
+        {
+            PlexPy: {
+                url: document.domain + ':8181',
+                image: 'assets/img/plexpy.png',
+                header: 'Server stats',
+                description: 'Want to know what\'s going on at <span class="server-name">SERVER</span> or just interested in the most viewed movie? View it here!'
+            },
+            Sonarr: {
+                url: document.domain + ':8989',
+                image: 'assets/img/sonarr.png',
+                header: 'Series watchlist',
+                description: 'View the list of series that currently being monitored.'
+            },
+            Radarr: {
+                url: document.domain + ':7878',
+                image: 'assets/img/radarr.png',
+                header: 'Movies watchlist',
+                description: 'View the list of movies that currently being monitored.'
+            }
         }
-    };
+    ];
 
     var linkBarElement = document.getElementById('link-bar');
     this.linkBarRow = linkBarElement.querySelector('.row');
@@ -93,9 +126,6 @@ PlexRedirect.prototype = {
         this.config = this.default_config;
         console.error('Failed to load config, using the default one');
         this.setSiteSettings();
-    },
-    isset: function(param) {
-        return (typeof param !== "undefined" && param !== "");
     },
     setSiteSettings: function() {
         this.createLinkBar();
@@ -114,57 +144,84 @@ PlexRedirect.prototype = {
                 serverNames[i].innerText = serverConfig.name;
             }
         }
+        if(typeof this.config.messages === "undefined") {
+            this.config.messages = this.default_messages;
+        }
 
         this.checkServer();
     },
     createLinkBar: function () {
-        this.numberOfApplications = Object.keys(this.config.applications).length;
+        if(typeof this.config.applications === "undefined") {
+            this.config.applications = [];
+        }
+
+        this.numberOfApplications = this.config.applications.length;
         if(this.numberOfApplications == 0) {
             this.config.applications = this.default_applications;
-            this.numberOfApplications = Object.keys(this.config.applications).length;
+            this.numberOfApplications = this.config.applications.length;
         }
 
-        for(var key in this.config.applications) {
-            if (!this.config.applications.hasOwnProperty(key)) continue;
+        for(var row in this.config.applications) {
+            if (!this.config.applications.hasOwnProperty(row)) continue;
 
-            this.createApplicationElement(this.config.applications[key], key);
+            this.createApplicationRow(row);
         }
     },
-    createApplicationElement: function(currentApplication, key) {
-        var div = document.createElement('div').addClass(this.getApplicationColumnWidth());
+    createApplicationRow: function(currentRow) {
+        var rowDiv = document.createElement('div').addClass('row');
+
+        var numberOfApplications = Object.keys(this.config.applications[currentRow]).length;
+
+        var columnWidht = Math.floor(12 / numberOfApplications);
+        var offset = Math.floor((12 % numberOfApplications) / 2);
+        if(offset == 0) {
+            offset = false;
+        }
+
+        for(var key in this.config.applications[currentRow]) {
+            if (!this.config.applications[currentRow].hasOwnProperty(key)) continue;
+
+            var element = this.createApplicationElement(currentRow, key, columnWidht, offset);
+            rowDiv.appendChild(element);
+
+            offset = false;
+        }
+        this.linkBarRow.appendChild(rowDiv);
+    },
+    createApplicationElement: function(row, key, columnWidht, offset) {
+        var currentApplication = this.config.applications[row][key];
+
+        var div = document.createElement('div').addClass('col-lg-' + columnWidht);
+        if(offset) {
+            div.addClass('col-lg-offset-' + offset);
+        }
         var link = document.createElement('a')
-            .attr('href', currentApplication.hasAttibute('url') ? currentApplication.url : this.default_applications[key].url)
+            .attr('href', currentApplication.hasAttibute('url') ? currentApplication.url : this.default_applications[row][key].url)
             .attr('target', '_top');
         var image = document.createElement('img')
-            .attr('src', currentApplication.hasAttibute('image') ? currentApplication.image : this.default_applications[key].image)
+            .attr('src', currentApplication.hasAttibute('image') ? currentApplication.image : this.default_applications[row][key].image)
             .attr('width', '180');
         var header = document.createElement('h4');
-        header.innerHTML = currentApplication.hasAttibute('header') ? currentApplication.header : this.default_applications[key].header;
+        header.innerHTML = currentApplication.hasAttibute('header') ? currentApplication.header : this.default_applications[row][key].header;
 
         var description = document.createElement('p');
-        description.innerHTML = currentApplication.hasAttibute('description') ? currentApplication.description : this.default_applications[key].description;
+        description.innerHTML = currentApplication.hasAttibute('description') ? currentApplication.description : this.default_applications[row][key].description;
 
         link.appendChild(image);
         link.appendChild(header);
         link.appendChild(description);
 
         div.appendChild(link);
-        this.linkBarRow.appendChild(div);
-    },
-    getApplicationColumnWidth: function() {
-        if(this.numberOfApplications <= 4) {
-            return 'col-lg-' + (12 / this.numberOfApplications);
-        }
-
-        return 'col-lg-4';
+        return div;
     },
     checkServer: function() {
         var p = new Ping();
-        var server = this.config.server.domain == "" ? document.domain : this.config.server.domain; //Try to get it automagically, but you can manually specify this
+        var server = this.config.server.plex_domain == "" ? document.domain : this.config.server.plex_domain; //Try to get it automagically, but you can manually specify this
+        var port = this.config.server.plex_port == "" ? "32400" : this.config.server.plex_port;
         var timeout = 2000; //Milliseconds
         var body = document.getElementsByTagName("body")[0];
         var _this = this;
-        p.ping(server+":32400", function(data) {
+        p.ping(server + ":" + port, function(data) {
             if (data < 1000){
                 _this.setServerMessage(_this.server_status.UP, _this.config.messages.server_up);
             }else{
